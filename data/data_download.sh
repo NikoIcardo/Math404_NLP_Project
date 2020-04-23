25f3bd4caac97f84cce2ac2b69c2a789b9e558b1
jpiaskowski <julia.piask@gmail.com>
2020-04-22 17:48:12 -0700

    data added

---
+++ data/data_download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/DavidGrangier/wikipedia-biography-dataset.git
+cd wikipedia-biography-dataset
+cat wikipedia-biography-dataset.z?? > tmp.zip
+unzip tmp.zip
+rm tmp.zip
+
